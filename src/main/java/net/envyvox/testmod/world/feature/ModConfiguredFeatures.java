99b60ba1feda6b5625002bd7f4d45f5b3e85529f
envyvox <envyvox@gmail.com>
2023-11-03 22:50:24 +0200

    #33 custom geode

--- src/main/java/net/envyvox/testmod/world/feature/ModConfiguredFeatures.java
+++ src/main/java/net/envyvox/testmod/world/feature/ModConfiguredFeatures.java
@@ -3,10 +3,17 @@ package net.envyvox.testmod.world.feature;
 import net.envyvox.testmod.TestMod;
 import net.envyvox.testmod.block.ModBlocks;
 import net.minecraft.core.Registry;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.levelgen.GeodeBlockSettings;
+import net.minecraft.world.level.levelgen.GeodeCrackSettings;
+import net.minecraft.world.level.levelgen.GeodeLayerSettings;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.WeightedPlacedFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.GeodeConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.RandomFeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
@@ -22,6 +29,22 @@ import java.util.List;
 public class ModConfiguredFeatures {
     public static final DeferredRegister<ConfiguredFeature<?, ?>> CONFIGURED_FEATURES =
             DeferredRegister.create(Registry.CONFIGURED_FEATURE_REGISTRY, TestMod.MOD_ID);
+
+    public static final RegistryObject<ConfiguredFeature<?, ?>> ZIRCON_GEODE = CONFIGURED_FEATURES.register(
+            "zircon_geode",
+            () -> new ConfiguredFeature<>(Feature.GEODE,
+                    new GeodeConfiguration(new GeodeBlockSettings(BlockStateProvider.simple(Blocks.AIR),
+                            BlockStateProvider.simple(Blocks.DEEPSLATE),
+                            BlockStateProvider.simple(ModBlocks.ZIRCON_ORE.get()),
+                            BlockStateProvider.simple(Blocks.DIRT),
+                            BlockStateProvider.simple(Blocks.EMERALD_BLOCK),
+                            List.of(ModBlocks.ZIRCON_BLOCK.get().defaultBlockState()),
+                            BlockTags.FEATURES_CANNOT_REPLACE, BlockTags.GEODE_INVALID_BLOCKS),
+                            new GeodeLayerSettings(1.7D, 1.2D, 2.5D, 3.5D),
+                            new GeodeCrackSettings(0.25D, 1.5D, 1), 0.5D, 0.1D,
+                            true, UniformInt.of(3, 8),
+                            UniformInt.of(2, 6), UniformInt.of(1, 2),
+                            -18, 18, 0.075D, 1)));
 
     public static final RegistryObject<ConfiguredFeature<?, ?>> RED_MAPLE =
             CONFIGURED_FEATURES.register("red_maple", () ->
--- src/main/java/net/envyvox/testmod/world/feature/ModPlacedFeatures.java
+++ src/main/java/net/envyvox/testmod/world/feature/ModPlacedFeatures.java
@@ -5,7 +5,8 @@ import net.envyvox.testmod.block.ModBlocks;
 import net.minecraft.core.Registry;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.data.worldgen.placement.VegetationPlacements;
-import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
+import net.minecraft.world.level.levelgen.placement.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
@@ -15,6 +16,13 @@ import java.util.List;
 public class ModPlacedFeatures {
     public static final DeferredRegister<PlacedFeature> PLACED_FEATURES =
             DeferredRegister.create(Registry.PLACED_FEATURE_REGISTRY, TestMod.MOD_ID);
+
+    public static final RegistryObject<PlacedFeature> ZIRCON_GEODE_PLACED = PLACED_FEATURES.register(
+            "zircon_geode_placed", () -> new PlacedFeature(
+                    ModConfiguredFeatures.ZIRCON_GEODE.getHolder().get(), List.of(
+                    RarityFilter.onAverageOnceEvery(50), InSquarePlacement.spread(),
+                    HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(6), VerticalAnchor.absolute(50)),
+                    BiomeFilter.biome())));
 
     public static final RegistryObject<PlacedFeature> RED_MAPLE_CHECKED = PLACED_FEATURES.register("red_maple_checked",
             () -> new PlacedFeature(ModConfiguredFeatures.RED_MAPLE.getHolder().get(),
